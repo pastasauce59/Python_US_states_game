b57ca433417c6a20c69ebd574fd6d6f6b7576b5b
Michael Primavera <pastasauce59@gmail.com>
2023-04-02 23:20:27 -0400

    Created screen textinput to take in user's typed answer and check if it exists in a list of states and return the state if it does.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -8,10 +9,23 @@ turtle.shape(image)
 
 
 
-def get_mouse_click_cor(x,y):
-    print(x, y)
+# def get_mouse_click_cor(x,y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_cor)
 
-turtle.onscreenclick(get_mouse_click_cor)
+data = pandas.read_csv("50_states.csv")
+# ny = data[data.state == "New York"]
+
+states_list = data['state'].to_list()
+# print(states_list)
+
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+if answer_state in states_list:
+    state = data[data.state == answer_state]
+    # turtle.goto(state.x, state.y)
+    # turtle.write(state.state)
+    print(state)
 
 #An alternative to screen.exitonclick()
 turtle.mainloop()
