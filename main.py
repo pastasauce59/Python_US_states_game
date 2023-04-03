6002522241f18e729058318fa7e31ca767d6aee5
Michael Primavera <pastasauce59@gmail.com>
2023-04-03 15:23:28 -0400

    Created list of x and y coordinates from 50_states.csv and refrenced their index to write the user's guess on the map. Functionality is still not working properply but the state will be writtin to the map.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+screen.setup(width=800, height=800)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -14,18 +15,21 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_cor)
 
 data = pandas.read_csv("50_states.csv")
-# ny = data[data.state == "New York"]
+ny = data[data.state == "New York"]
 
 states_list = data['state'].to_list()
-# print(states_list)
-
+x_list = data['x'].to_list()
+y_list = data['y'].to_list()
 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
 if answer_state in states_list:
-    state = data[data.state == answer_state]
-    # turtle.goto(state.x, state.y)
-    # turtle.write(state.state)
-    print(state)
+    state_index = states_list.index(answer_state)
+    state_x_cor = x_list[state_index]
+    state_y_cor = y_list[state_index]
+    turtle.penup()
+    turtle.write(answer_state)
+    turtle.goto(-state_x_cor, -state_y_cor)
+    # print(state_x)
 
 #An alternative to screen.exitonclick()
 turtle.mainloop()
