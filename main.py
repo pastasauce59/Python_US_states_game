b9cf805f51e259bd64980cf63cb5981884f333e7
Michael Primavera <pastasauce59@gmail.com>
2023-04-05 14:38:16 -0400

    Reduced code for 'states_to_learn' list by using list comprehension, reducing it to only 2 lines of code.

--- main.py
+++ main.py
@@ -21,12 +21,14 @@ correct_state_guesses = []
 while len(correct_state_guesses) != 50:
     answer_state = screen.textinput(title=f"{len(correct_state_guesses)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == 'Exit':
-        states_to_learn = []
-        for state in states_list:
-            if state not in correct_state_guesses:
-                states_to_learn.append(state)
-        states_to_learn_data = pandas.DataFrame(states_to_learn)
-        states_to_learn_data.to_csv("states_to_learn.csv")
+        # states_to_learn = []
+        # for state in states_list:
+        #     if state not in correct_state_guesses:
+        #         states_to_learn.append(state)
+        # states_to_learn_data = pandas.DataFrame(states_to_learn)
+        # states_to_learn_data.to_csv("states_to_learn.csv")
+        states_to_learn = [state for state in states_list if state not in correct_state_guesses]
+        print(states_to_learn)
         break
     if answer_state in states_list:
         state_data = data[data.state == answer_state]
